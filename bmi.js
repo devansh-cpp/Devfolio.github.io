fbb32a3db24befade89d61668e6f5ac573a136ea
Devansh Mishra <113198313+devansh-cpp@users.noreply.github.com>
2024-01-09 10:17:35 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+
+    const results = document.querySelector('.results')
+
+    if(height=== '' || height <0 || isNaN(height)){
+        results.innerHTML = `Please Enter the height ${height}`
+    }
+     
+    else if(weight=== '' || weight <0 || isNaN(weight)){
+        results.innerHTML = `Please Enter the height ${weight}`
+    }
+    else{
+       const bmi = (weight/((height*height)/10000)).toFixed(2)
+       if(bmi<18.6){
+        results.innerHTML=`<span>${bmi} You are Under Weight</span>`
+        document.body.style.backgroundColor="skyblue"
+       }
+       if(bmi>18.6 && bmi<24.9){
+        results.innerHTML=`<span>${bmi} You are Normal Weight</span>`
+        document.body.style.backgroundColor="lightgreen"
+       }
+       if(bmi>24.9){
+        results.innerHTML=`<span>${bmi} You are over Weight</span>`
+        document.body.style.backgroundColor="yellow"
+
+       }
+     }
+
+
+})
